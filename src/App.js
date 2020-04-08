5d88208f81b36e2891d481ca98b79b1a295e1115
Панов Илья <panov210@yandex.ru>
2020-04-08 10:43:20 +0300

    Реализовал логику

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Alignment, Button } from "@blueprintjs/core";
+import { Table, Column, Cell } from "@blueprintjs/table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchValue: '',
+      items: ['apple', 'banana', 'cherry'],
+    };
+
+    this.onSearchValueChanged = this.onSearchValueChanged.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.renderCell = this.renderCell.bind(this);
+  }
+
+  onSearchValueChanged(e) {
+    this.setState({searchValue: e.target.value});
+  }
+
+  addItem() {
+    this.state.items.push(this.state.searchValue);
+  }
+
+  renderCell(rowIndex) {
+    const items = this.state.searchValue ? this.state.items.filter(x => x.includes(this.state.searchValue)) : this.state.items;
+    return <Cell>{items[rowIndex]}</Cell>;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar align={Alignment.CENTER}>
+          <Navbar.Group>
+            <input type="search" value={this.state.searchValue} onChange={this.onSearchValueChanged} placeholder="Поиск..." style={{marginRight: '5px'}} />
+            <Button icon="add" onClick={this.addItem} />
+            <Navbar.Divider />
+            <Navbar.Heading>Arbina</Navbar.Heading>
+          </Navbar.Group>
+        </Navbar>
+        <Table numRows={this.state.items.length}>
+            <Column name="Данные" cellRenderer={this.renderCell} />
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default App;
